c05f3be39a439a4331e9b8c0565557a6efc63637
AlexandrPozhidaev <alexandrpojidaev@andex.ru>
2026-03-16 20:26:14 +0300

    Чекстайл - отделил интерфейсы друг от друга строкой

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -14,13 +14,16 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class User {
 
-    public interface OnCreate {}
-    public interface OnUpdate {}
+    public interface OnCreate {
+    }
+
+    public interface OnUpdate {
+    }
 
     private Long id;
 
-    @NotBlank(message = "Электронная почта не может быть пустой",groups = OnCreate.class)
-    @Email(message = "Некорректный формат электронной почты",groups = {OnCreate.class, OnUpdate.class})
+    @NotBlank(message = "Электронная почта не может быть пустой", groups = OnCreate.class)
+    @Email(message = "Некорректный формат электронной почты", groups = {OnCreate.class, OnUpdate.class})
     private String email;
 
     @NotBlank(message = "Логин не может быть пустым", groups = {OnCreate.class, OnUpdate.class})
