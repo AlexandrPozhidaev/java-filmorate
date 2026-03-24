3a7b7769494fd70df643544db8e57b92ca1e06ea
AlexandrPozhidaev <alexandrpojidaev@andex.ru>
2026-03-25 01:47:12 +0300

    Добавил интервалы между строк ы storage

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -9,10 +9,16 @@ import java.util.Optional;
 public interface FilmStorage {
 
     Film create(Film film);
+
     Film update(Film film);
+
     List<Film> getAll();
+
     void addLike(Long filmId, Long userId);
+
     boolean deleteLike(Long filmId, Long userId) throws BadRequestException;
+
     List<Film> getPopularFilms(int count);
+
     Optional<Film> getById(Long id);
     }
