93a1ded2c0a4d28099278181c2d05043df9a1cef
AlexandrPozhidaev <alexandrpojidaev@andex.ru>
2026-04-20 23:13:15 +0300

    Убрал очистку БД после каждого теста

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate;
 
 import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -28,14 +27,6 @@ class FilmorateApplicationTests {
 
     private final DbFilmStorage filmStorage;
     private final JdbcTemplate jdbcTemplate;
-
-    @AfterEach
-    void setUp() {
-        jdbcTemplate.update("DELETE FROM film_likes");
-        jdbcTemplate.update("DELETE FROM user_friends");
-        jdbcTemplate.update("DELETE FROM films");
-        jdbcTemplate.update("DELETE FROM users");
-    }
 
     @Test
     public void testFindUserById() {
